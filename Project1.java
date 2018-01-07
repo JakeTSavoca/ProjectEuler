55cb9814b0b65fbd7306431cee21f1db8c6e6d61
Jake T. Savoca <18jts2@fuji.cs.williams.edu>
2018-01-07 16:01:34 -0500

    change made

--- Project1.java
+++ Project1.java
@@ -1,5 +1,24 @@
+//If we list all the natural numbers below 10 that are multiples of 3 or 5,
+//we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+//Find the sum of all the multiples of 3 or 5 below 1000.
+    
+
 public class Project1{
     public static void main(String[] Args){
-	System.out.println( "soon to start project 1");
+	System.out.println( getMultiples(100) );
     }
+
+    //find all multiples of 3 and 5 below a certain number
+    public static int getMultiples(int number){
+	int sum = 0; 
+	//create loop
+	for (int i=0; i < 1000; i++){
+	    if ((i%3 == 0) || (i%5 == 0)){
+		sum = i + sum;
+	    }
+	}
+	return sum; 
+    }
+    
 }
