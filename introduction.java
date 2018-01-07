b26c25f0275bff61dc35b6b76422ef0fadae35b9
Jake T. Savoca <18jts2@fuji.cs.williams.edu>
2018-01-07 15:39:36 -0500

    making intro

---
+++ introduction.java
@@ -0,0 +1,15 @@
+public class Project1{
+    public static void main(String[] Args){
+	System.out.println( "printing");
+    }
+}
+
+// git clone (with URL of repository)
+// git add (file)
+//git commit -m "message"
+//git push
+//enter user and pass
+
+//if two copies of repository, must pull edits with ::: git pull
+
+
